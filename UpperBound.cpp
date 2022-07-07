29c9c14ee48ca864838dc7653a74d8a8838530aa
Sourav Ghosh <39117406+isourav545@users.noreply.github.com>
2022-07-07 15:36:30 +0530

    Create UpperBound.cpp

---
+++ UpperBound.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+#define setbits(n)__builtin_popcountll(n)
+
+
+
+int up(int arr[],int n, int x) {
+	int ans=n;
+	int low=0,high=n-1;
+	while(low<=high){
+		int mid = (low+high)/2;
+		if(arr[mid]<=x){
+			low=mid+1;
+		}
+		else{
+			ans=mid;
+			high=mid-1;
+		}
+	}
+	return ans;
+}
+signed main(){
+	#ifndef ONLINE_JUDGE
+	freopen("input.txt","r",stdin);
+	freopen("output.txt","w",stdout);
+	#endif
+	int n;
+	cin>>n;
+	int arr[n];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+	int x;
+	cin>>x;
+	cout<<up(arr,n,x);
+	
+	
+}
